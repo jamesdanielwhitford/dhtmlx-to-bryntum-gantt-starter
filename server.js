5b32cedcd44ea78d1c3000c583605c4adcc95dec
jamesdanielwhitford <70632508+jamesdanielwhitford@users.noreply.github.com>
2022-11-11 07:52:40 +0200

    Add complete code

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
+const uuid = require("uuid");
 require("dotenv").config();
 
 const port = process.env.PORT || 1338;
 const app = express();
 
 app.use(express.static(path.join(__dirname, "public")));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.listen(port, () => {
   console.log("Server is running on port " + port + "...");
@@ -26,186 +27,256 @@ const db = mysql.createPool({
 
 async function serverConfig() {
 
+
   app.get("/data", async (req, res) => {
     try {
       const results = await Promise.all([
-        db.query("SELECT * FROM gantt_tasks"),
-        db.query("SELECT * FROM gantt_links"),
+        db.query("SELECT * FROM tasks"),
+        db.query("SELECT * FROM dependencies"),
       ]);
-      const tasks = results[0][0],
-        links = results[1][0];
 
-      for (let i = 0; i < tasks.length; i++) {
-        tasks[i].start_date = tasks[i].start_date.format(
-          "YYYY-MM-DD hh:mm:ss"
-        );
-        tasks[i].open = true;
-      }
+      const tasks = results[0][0],
+        dependencies = results[1][0];
 
       res.send({
-        data: tasks,
-        collections: { links: links },
+        success: true,
+        tasks: {
+          rows: tasks,
+        },
+        dependencies: {
+          rows: dependencies,
+        },
       });
     } catch (error) {
-      sendResponse(res, "error", null, error);
+      sendResponse(res, "error", null, error, [], [], [], []);
     }
   });
 
-  // add a new task
-  app.post("/data/task", async (req, res) => {
-    const task = getTask(req.body);
-    try {
-      // find task with highest sortorders
-      const result = await db.query(
-        "SELECT MAX(sortorder) AS maxOrder FROM gantt_tasks"
-      );
-      // assign max sort order to new task
-      const orderIndex = (result[0][0].maxOrder || 0) + 1;
-      try {
-        await db.query(
-          "INSERT INTO gantt_tasks(text, start_date, duration," +
-            "progress, parent, sortorder) VALUES (?,?,?,?,?,?)",
-          [
-            task.text,
-            task.start_date,
-            task.duration,
-            task.progress,
-            task.parent,
-            orderIndex,
-          ]
-        );
-        sendResponse(res, "inserted", result.insertId);
-      } catch (error) {
-        sendResponse(res, "error", null, error);
+
+  // Create, Update, Delete (Tasks & Dependencies)
+  app.post("/api", async function (req, res) {
+    let requestId = "";
+    let lastKey = "";
+    let err = null;
+
+    const taskUpdates = [];
+    const tasksRemoved = [];
+    const dependencyUpdates = [];
+    const dependenciesRemoved = [];
+
+    console.log(
+      `\n${req.method} ${req.url} --> ${JSON.stringify(
+        req.body,
+        `\t`,
+        2
+      )}`
+    );
+
+    for (const [key, value] of Object.entries(req.body)) {
+      if (key === "requestId") {
+        requestId = value;
       }
-    } catch (error) {
-      sendResponse(res, "error", null, error);
+      if (key === "tasks") {
+        for (const [key2, value2] of Object.entries(value)) {
+          if (key2 === "added") {
+            value2.forEach((addObj) => taskUpdates.push(addObj));
+            value2[0].id = uuid.v4();
+            const val = await createOperation(value2[0], "tasks");
+            lastKey = val.msg;
+            err = val.error;
+          }
+
+          if (key2 === "updated") {
+            console.log(`updated tasks...`, value2);
+            value2.forEach((updateObj) =>
+              taskUpdates.push(updateObj)
+            );
+            const val = await updateOperation(value2, "tasks");
+            lastKey = val.msg;
+            err = val.error;
+          }
+
+          if (key2 === "removed") {
+            tasksRemoved.push(value2[0]);
+            const val = await deleteOperation(value2[0].id, "tasks");
+            lastKey = val.msg;
+            err = val.error;
+          }
+        }
+      }
+
+      if (key === "dependencies") {
+        for (const [key2, value2] of Object.entries(value)) {
+          if (key2 === "added") {
+            value2[0].id = uuid.v4();
+            value2.forEach((addObj) =>
+              dependencyUpdates.push(addObj)
+            );
+            const val = await createOperation(
+              value2[0],
+              "dependencies"
+            );
+            lastKey = val.msg;
+            err = val.error;
+          }
+
+          if (key2 === "updated") {
+            value2.forEach((updateObj) =>
+              dependencyUpdates.push(updateObj)
+            );
+            const val = await updateOperation(value2, "dependencies");
+            lastKey = val.msg;
+            err = val.error;
+          }
+
+          if (key2 === "removed") {
+            dependenciesRemoved.push(value2[0]);
+            const val = await deleteOperation(
+              value2[0].id,
+              "dependencies"
+            );
+            lastKey = val.msg;
+            err = val.error;
+          }
+        }
+      }
     }
+
+
+    sendResponse(
+      res,
+      lastKey,
+      requestId,
+      err,
+      taskUpdates,
+      dependencyUpdates,
+      tasksRemoved,
+      dependenciesRemoved
+    );
   });
+}
 
-  // update a task
-  app.put("/data/task/:id", async (req, res) => {
-    const sid = req.params.id,
-      target = req.body.target,
-      task = getTask(req.body);
-    try {
-      await Promise.all([
-        db.query(
-          "UPDATE gantt_tasks SET text = ?, start_date = ?," +
-            "duration = ?, progress = ?, parent = ? WHERE id = ?",
-          [
-            task.text,
-            task.start_date,
-            task.duration,
-            task.progress,
-            task.parent,
-            sid,
-          ]
-        ),
-        updateOrder(sid, target),
-      ]);
-      sendResponse(res, "updated");
-    } catch (error) {
-      sendResponse(res, "error", null, error);
+async function createOperation(addObj, table) {
+  const valArr = [];
+  const keyArr = [];
+  for (const [key, value] of Object.entries(addObj)) {
+    if (
+      key !== "baselines" &&
+      key !== "from" &&
+      key !== "to" &&
+      key !== "$PhantomId" &&
+      key !== "segments" &&
+      key !== "ignoreResourceCalendar"
+    ) {
+      keyArr.push(`\`${key}\``);
+      valArr.push(value);
     }
-  });
-
-  async function updateOrder(taskId, target) {
-    let nextTask = false;
-    let targetOrder;
-
-    if (target.startsWith("next:")) {
-      target = target.substring("next:".length);
-      nextTask = true;
-    }
-
-    try {
-      const result = await db.query(
-        "SELECT * FROM gantt_tasks WHERE id = ?",
-        [target]
-      );
-      if (!result[0][0]) return;
-      targetOrder = result[0][0].sortorder;
-      if (nextTask) targetOrder++;
-      try {
-        await db.query(
-          "UPDATE gantt_tasks SET sortorder" +
-            " = sortorder + 1 WHERE sortorder >= ?",
-          [targetOrder]
-        );
-        return db.query(
-          "UPDATE gantt_tasks SET sortorder = ? WHERE id = ?",
-          [targetOrder, taskId]
-        );
-      } catch (error) {}
-    } catch (error) {}
   }
 
-  // delete a task
-  app.delete("/data/task/:id", async (req, res) => {
-    const sid = req.params.id;
-    try {
-      await db.query("DELETE FROM gantt_tasks WHERE id = ?", [sid]);
-      sendResponse(res, "deleted");
-    } catch (error) {
-      sendResponse(res, "error", null, error);
-    }
-  });
-
-  // add a link
-  app.post("/data/link", async (req, res) => {
-    const link = getLink(req.body);
-    try {
-      await db.query(
-        "INSERT INTO gantt_links(source, target, type) VALUES (?,?,?)",
-        [link.source, link.target, link.type]
-      );
-      sendResponse(res, "linked", result.insertId);
-    } catch (error) {
-      sendResponse(res, "error", null, error);
-    }
-  });
-
-  // delete a link
-  app.delete("/data/link/:id", async (req, res) => {
-    const sid = req.params.id;
-    try {
-      await db.query("DELETE FROM gantt_links WHERE id = ?", [sid]);
-      sendResponse(res, "deleted");
-    } catch (error) {
-      sendResponse(res, "error", null, error);
-    }
-  });
-
-  function getTask(data) {
-    return {
-      text: data.text,
-      start_date: data.start_date.date("YYYY-MM-DD"),
-      duration: data.duration,
-      progress: data.progress || 0,
-      parent: data.parent,
-    };
-  }
-
-  function getLink(data) {
-    return {
-      source: data.source,
-      target: data.target,
-      type: data.type,
-    };
+  try {
+    await db.query(
+      `INSERT INTO ${table} (${keyArr.join(
+        ", "
+      )}) VALUES (${Array(keyArr.length).fill("?").join(",")})`,
+      valArr
+    );
+    return { msg: "added", error: null };
+  } catch (error) {
+    return { msg: "error", error: error };
   }
 }
 
-serverConfig();
+async function updateOperation(updates, table) {
+  try {
+    await Promise.all(
+      updates.map(({ id, ...update }) => {
+        return db.query(
+          `
+          UPDATE ${table}
+          SET ${Object.keys(update)
+            .map((key) => `${key} = ?`)
+            .join(", ")}
+          WHERE id = ?
+        `,
+          Object.values(update).concat(id)
+        );
+      })
+    );
+    return { msg: "update", error: null };
+  } catch (error) {
+    return { msg: "error", error };
+  }
+}
 
-function sendResponse(res, action, requestId, error) {
+async function deleteOperation(id, table) {
+  try {
+    await db.query(`DELETE FROM ${table} WHERE id = ?`, [id]);
+    return { msg: "deleted", error: null };
+  } catch (error) {
+    return { msg: "error", error: error };
+  }
+}
+
+
+function sendResponse(
+  res,
+  action,
+  requestId,
+  error,
+  taskUpdates,
+  dependencyUpdates,
+  tasksRemoved,
+  dependenciesRemoved
+) {
   if (action == "error") console.log(error);
 
   const result = {
     success: action === "error" ? false : true,
   };
-  if (requestId) result.requestId = requestId;
+  if (requestId !== undefined && requestId !== null)
+    result.requestId = requestId;
+
+  // updated tasks
+  result.tasks = {};
+  result.tasks.rows = [];
+
+  if (taskUpdates.length) {
+    result.tasks.rows = [...result.tasks.rows, ...taskUpdates];
+  }
+
+  // deleted tasks
+  result.tasks.removed = [];
+
+  if (tasksRemoved.length) {
+    result.tasks.removed = [
+      ...result.tasks.removed,
+      ...tasksRemoved,
+    ];
+  }
+
+  // updated dependencies
+  result.dependencies = {};
+  result.dependencies.rows = [];
+
+  if (dependencyUpdates.length) {
+    result.dependencies.rows = [
+      ...result.dependencies.rows,
+      ...dependencyUpdates,
+    ];
+  }
+
+  // deleted dependencies
+  result.dependencies.removed = [];
+
+  if (dependenciesRemoved.length) {
+    result.dependencies.removed = [
+      ...result.dependencies.removed,
+      ...dependenciesRemoved,
+    ];
+  }
 
   res.send(result);
   return;
 }
+
+serverConfig();
